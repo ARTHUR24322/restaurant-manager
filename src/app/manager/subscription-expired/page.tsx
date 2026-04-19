d18dd1fc060356a9693fc02b083677014ecc98fd
Arthur Kisumbule <Kisumbulearthur@gmail.com>
2026-04-19 04:37:27 +0100

    fix build error

--- src/app/manager/subscription-expired/page.tsx
+++ src/app/manager/subscription-expired/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React from "react";
 import { getManagerSession } from "@/lib/manager-actions";
 import { SubscriptionExpiredClient } from "@/components/manager/SubscriptionExpiredClient";
